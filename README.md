f616f70f1efb86aa8f6bd8692b8968bb2a6c94fb
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2024-03-27 16:47:21 +0800

    Update the read me file.

--- README.md
+++ README.md
@@ -1,14 +1,73 @@
-# M221-PLC-Simulator
+# Python Virtual PLC & RTU Simulator
 
-**Program Design Purpose**: We want to create a PLC simulator which can let the user to draw ladder diagram, then simulate the PLC's physical reaction (digital in/out) when loaded the ladder logic in the PLC, it also provide the ModBus-TCP interface for other module to read the current PLC state and do the control. [It is a under going project, not finished yet!]
+**Program Design Purpose**: The objective of this program is create a python program which can simulate the fundamental function of two kinds of common industrial automation OT-devices, PLC (Programmable Logic Controller) and RTU (Remote Terminal Unit). The program can run on different OS to convert a VM/Machine to be a virtual PLC/RTU, which can handle the request from SCADA-HMI system via Modbus TCP or S7Comm, then simulate the real PLC/RTU device's electrical signal changes then use UDP to feed the changes to the real-world emulation program. The project contents two main project:
 
- 
+- **PLC Simulation System [ `Modbus` ]**: A PLC simulate system follow the operation logic of the  Schneider M221 PLC with the Modbus-TCP client, Modbus-TCP server, Ladder logic simulation, PLC register-memory-coil control,Real-world emulator components connection interface. 
+- **RTU Simulation System [ `S7Comm` ]**: A PLC and RTU simulation system follow the operation logic of the Siemens Simatic S7-1200 PLC and SIMATIC RTU3000C with the S7comm client, S7comm server, PLC/RTU memory management, Ladder logic / RTU logic simulation, real-world emulator components connection interface.
+
+
+
+```
+# Created:     2024/02/21
+# Copyright:   Copyright (c) 2024 LiuYuancheng
+# License:     MIT License
+```
+
+**Table of Contents**
 
 [TOC]
 
 ------
 
 ### Introduction
+
+
+
+We developed virtual PLC emulator program which can auto run simple ladder logic and provided below customized function for flexible usage: 
+
+- Emulate Schneider M22X Modbus-TCP [port 502] Communication protocol 
+- Emulate Siemens S71200 S7comm [port 102] Communication protocol 
+- Customized software define ladder diagram execution priority 
+- UDP interface for electrical signal (such as voltage) connection emulation to connect to the real world.
+- Simulate the PLC ladder logic execution with customizable time clock cycle configuration for education purpose. 
+- Simulate the multiple PLCs master-slave connection (DCM-DCM with RS422 multi-dop connection).
+- Simulate the PLC access limitation (IP addresses allow read list / allow set list) config. 
+
+
+
+------
+
+### Background Knowledge 
+
+##### Difference Between Modbus TCP and S7comm 
+
+Modbus TCP and S7Comm are both communication protocols used in OT environment industrial automation, but they are associated with different manufacturers and have some differences in their features and implementations:
+
+1. **Origin and Manufacturers:**
+   - **Modbus TCP:** Modbus is an open-source protocol developed by Modicon (now Schneider Electric). Modbus TCP is an Ethernet-based implementation of the Modbus protocol.
+   - **S7Comm:** S7Comm is a protocol developed by Siemens for communication with their programmable logic controllers (PLCs), primarily in the Simatic S7 series.
+2. **Vendor Support:**
+   - **Modbus TCP:** Being an open protocol, Modbus TCP is supported by a wide range of industrial automation equipment manufacturers.
+   - **S7Comm:** S7Comm is proprietary to Siemens, so it's primarily used with Siemens PLCs and devices.
+3. **Functionality and Features:**
+   - **Modbus TCP:** Modbus TCP is relatively simple and lightweight, making it easy to implement and suitable for basic communication needs in industrial automation. It supports functions such as reading and writing data registers, reading input registers, and controlling discrete outputs.
+   - **S7Comm:** S7Comm is more feature-rich and comprehensive, offering advanced functionality tailored specifically for Siemens PLCs. It supports a broader range of data types, diagnostic capabilities, and features like access to PLC hardware information.
+4. **Performance and Efficiency:**
+   - **Modbus TCP:** Modbus TCP tends to be simpler and lighter, which can result in lower overhead and faster communication in certain scenarios, especially for smaller-scale systems.
+   - **S7Comm:** S7Comm may offer better performance and efficiency in larger and more complex industrial automation environments due to its optimized design for Siemens PLCs.
+5. **Security:**
+   - **Modbus TCP:** Modbus TCP lacks built-in security features, although it can be used over VPNs or in conjunction with additional security measures to secure communication.
+   - **S7Comm:** Siemens has implemented various security features in S7Comm, such as encryption and authentication, to ensure secure communication between devices.
+
+In summary, while both Modbus TCP and S7Comm serve similar purposes in industrial automation, they differ in terms of their origin, vendor support, functionality, performance, and security features. The choice between them often depends on factors such as the specific requirements of the automation system, the compatibility with existing equipment, and the preferences of the system integrator or end-user.
+
+
+
+
+
+
+
+
 
 This is the main simulator UI:
 
--- S7Comm_RTU_Simulator/src/rtuSimulator.py
+++ S7Comm_RTU_Simulator/src/rtuSimulator.py
@@ -14,6 +14,142 @@
 # License:     MIT License
 #-----------------------------------------------------------------------------
 
+import time
+import json
+import threading
+from datetime import datetime
+from collections import OrderedDict
+
+import Log
+
+import udpCom
 import snap7Comm
 
+
+RECON_INT = 30 # reconnection time interval default set 30 sec
+
+# Define all the module local untility functions here:
+#-----------------------------------------------------------------------------
+def parseIncomeMsg(msg):
+    """ parse the income message to tuple with 3 element: request key, type and jsonString
+        Args: msg (str): example: 'GET;dataType;{"user":"<username>"}'
+    """
+    req = msg.decode('UTF-8') if not isinstance(msg, str) else msg
+    reqKey = reqType = reqJsonStr= None
+    try:
+        reqKey, reqType, reqJsonStr = req.split(';', 2)
+    except Exception as err:
+        Log.error('parseIncomeMsg(): The income message format is incorrect.')
+        Log.exception(err)
+    return (reqKey.strip(), reqType.strip(), reqJsonStr)
+
+#-----------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
+class RealWorldConnector(object):
+    """ A UDP connector used to connect to the realword to fetech the realworld 
+        PLC input data and 
+
+        Args:
+            object (_type_): _description_
+    """
+    def __init__(self, parent, address) -> None:
+        self.parent = parent
+        self.address = address
+        self.realwordInfo= {
+            'ip': address[0],
+            'port': address[1]
+        }
+        self.rwConnector = udpCom.udpClient((self.realwordInfo['ip'], self.realwordInfo['port']))
+        self.recoonectCount = RECON_INT
+        # Test login the real world emulator
+        self.plcID = self.parent.getPlcID()
+        self.realworldOnline = self._loginRealWord(plcID= self.plcID)
+        connMsg = 'Login the realworld successfully' if self.realworldOnline else 'Cannot connect to the realworld emulator'
+        Log.info(connMsg)
+
+    def isRealWorldOnline(self):
+        return self.realworldOnline
+
+#-----------------------------------------------------------------------------
+    def _loginRealWord(self, plcID=None):
+        """ Try to connect to the realworld emulator with the plc ID."""
+        Log.info("Try to connnect to the realword [%s]..." %str(self.address))
+        rqstKey = 'GET'
+        rqstType = 'login'
+        rqstDict = {'plcID': plcID}
+        result = self._queryToRW(rqstKey, rqstType, rqstDict)
+        if result:
+            Log.info("Realworld emulator online, state: ready")
+            return True
+        return False
+
+#-----------------------------------------------------------------------------
+    def reConnectRW(self):
+        """ Try to reconnect to the real world emulator."""
+        if self.recoonectCount <= 0:
+            Log.info('Try to reconnect to the realword.')
+            self.realworldOnline = self._loginRealWord(plcID=self.plcID)
+            self.recoonectCount = RECON_INT
+        self.recoonectCount -= 1
+
+#-----------------------------------------------------------------------------
+    def fetchRWInputData(self, rqstType='input', inputDict={}):
+        """ Fetch the input data from the realworld emulator. 
+            return: 
+                (key, rqstType, inputResultDict)
+        """
+        rqstKey = 'GET'
+        if isinstance(inputDict, dict):
+            return self._queryToRW(rqstKey, rqstType, inputDict)
+        Log.warning("getRWInputData(): passed in input parm needs to be a dict() type.")
+        return None
+
+#-----------------------------------------------------------------------------
+    def changeRWCoil(self, rqstType='signals', coilDict={}):
+        """ Send the current plc coils state to the realwrold emulator."""
+        rqstKey = 'POST'
+        print(coilDict)
+        if isinstance(coilDict, dict):
+            return self._queryToRW(rqstKey, rqstType, coilDict)
+        else:
+            Log.warning("changeRWCoil(): passed in input parm needs to be a dict() type.get %s" %str(coilDict))
+            return None
+
+#-----------------------------------------------------------------------------
+    def _queryToRW(self, rqstKey, rqstType, rqstDict, response=True):
+        """ Query message to realword emulator
+            Args:
+                rqstKey (str): request key (GET/POST/REP)
+                rqstType (str): request type string.
+                rqstDict (doct): request detail dictionary.
+                dataOnly (bool, optional): flag to indentify whether only return the 
+                    data. Defaults to True. return (responseKey, responseType, responseJson) if set
+                    to False.
+        Returns:
+            _type_: refer to <dataOnly> flag's explaination.
+        """
+        k = t = result = None
+        if rqstKey and rqstType and rqstDict:
+            rqst = ';'.join((rqstKey, rqstType, json.dumps(rqstDict)))
+            if self.rwConnector:
+                resp = self.rwConnector.sendMsg(rqst, resp=response)
+                if resp:
+                    #gv.gDebugPrint('===> resp:%s' %str(resp), logType=gv.LOG_INFO)
+                    k, t, data = parseIncomeMsg(resp)
+                    if k != 'REP': Log.warning('The msg reply key %s is invalid' % k)
+                    if t != rqstType: Log.warning('The reply type doesnt match.%s' %str((rqstType, t)))
+                    try:
+                        result = json.loads(data)
+                        self.lastUpdateT = datetime.now()
+                    except Exception as err:
+                        Log.exception('Exception: %s' %str(err))
+                        return None
+                else:
+                    Log.warning("Lost connection to the server.")
+                    self.realworldOnline = False
+                    return None
+        else:
+            Log.error("queryBE: input missing: %s" %str(rqstKey, rqstType, rqstDict))
+        return (k, t, result)
+    
 
