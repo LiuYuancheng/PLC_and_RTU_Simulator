2a58bef5e0bce2539a1c4f92ba4bbdade9f6e5ca
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2026-01-07 10:10:19 +0800

    update the bacnet communication module.

--- BACnet_PLC_Simulator/src/BACnetComm.py
+++ BACnet_PLC_Simulator/src/BACnetComm.py
@@ -1,30 +1,38 @@
-#!/usr/bin/env python
-"""
-Simple BACnet Server Example
-Hosts multiple analog value objects that can be read/written
-"""
+#!/usr/bin/python
+#-----------------------------------------------------------------------------
+# Name:        BACnetComm.py
+#
+# Purpose:     This module will use the BACpypes library (https://bacpypes.readthedocs.io/en/latest/index.html)
+#              to create a ISO 16484-5 Building Automation and Control Networks (BACnet) 
+#              server and client module communication API  test or simulate the 
+#              data/cmd flow connection of diverse building automation systems, 
+#              like HVAC, lighting, security, and access control,
+#
+# Author:      Yuancheng Liu
+#
+# Created:     2026/01/05
+# Version:     v_0.0.2
+# Copyright:   Copyright (c) 2026 Liu Yuancheng
+# License:     MIT License
+#-----------------------------------------------------------------------------
+
 import sys
-from bacpypes.core import run
+import time
+from threading import Thread, Event
+from bacpypes.core import run, stop
+from bacpypes.iocb import IOCB
+from bacpypes.constructeddata import Any
 from bacpypes.primitivedata import Real
 from bacpypes.object import AnalogValueObject, register_object_type
 from bacpypes.app import BIPSimpleApplication
-from bacpypes.local.device import LocalDeviceObject
-
-from bacpypes.core import run, stop
 from bacpypes.pdu import Address
-from bacpypes.app import BIPSimpleApplication
-from bacpypes.primitivedata import Real
 from bacpypes.apdu import ReadPropertyRequest, WritePropertyRequest
-from bacpypes.constructeddata import Any
-from bacpypes.iocb import IOCB
-from threading import Thread, Event
-import time
-
+from bacpypes.local.device import LocalDeviceObject
 
 DEF_SERVER_IP = "0.0.0.0"
 DEF_SERVER_PORT = 47808
 
-DEF_CLIENT_IP = "0.0.0.0/24"  # Change to your client machine's IP
+DEF_CLIENT_IP = "0.0.0.0/24"
 DEF_CLIENT_PORT = 47809
 
 DEF_ANALOG_VALUE_TYPE = 'analogValue'
@@ -33,8 +41,17 @@ DEF_PROPERTY_IDENTIFIER = 'presentValue'
 #-----------------------------------------------------------------------------
 #-----------------------------------------------------------------------------
 class BACnetServer(object):
-
+    """ The BAC server module to simulate a device or a sensors gateway in the 
+        BACnet network for the client to fetch the data.
+    """
     def __init__(self, deviceID, deviceName, ipaddr=DEF_SERVER_IP, port=DEF_SERVER_PORT):
+        """ Init Example: server = BACnetServer(123456, "TestDevice")
+            Args:
+                deviceID (int): the device unique ID.
+                deviceName (str): 
+                ipaddr (str, optional): host IP. Defaults to DEF_SERVER_IP.
+                port (int, optional): host port. Defaults to DEF_SERVER_PORT.
+        """
         self.deviceID = int(deviceID)
         self.deviceName = str(deviceName)
         self.deviceIP = "%s:%s" % (str(ipaddr), str(port))
@@ -46,11 +63,19 @@ class BACnetServer(object):
             vendorIdentifier=15,
         )
         self.application = BIPSimpleApplication(self.deviceInfo, self.deviceIP)
-        print("BACnetServer: Start device server with ip %s" % self.deviceIP)
-        self.analogObjDict = {} # init the analog object dict
+        print("BACnetServer: Start device server with ip %s" % str(self.deviceIP))
+        self.analogObjDict = {}  # init the analog object dict
 
     #-----------------------------------------------------------------------------
     def addAnalogObject(self, objName, objID, objValue, objDesc, objUnit):
+        """  Add an analog object to the BACnet server.
+            Args:
+                objName (str): object name. example: "objectName": "Temperature"
+                objID (int): unique object ID. example: "objectID": 1
+                objValue (float): init present value. example: "presentValue": 22.5,
+                objDesc (str): object description. example: "description": "Room Temperature",
+                objUnit (str): object unit. example: "units": "degreesCelsius"
+        """
         analogObj = AnalogValueObject(
             objectIdentifier=("analogValue", objID),
             objectName=objName,
@@ -61,7 +86,7 @@ class BACnetServer(object):
         )
         self.application.add_object(analogObj)
         self.analogObjDict[objName] = analogObj
-        print("BACnetServer: Add analog object %s" % objName)
+        print("BACnetServer: Added analog object %s" % objName)
 
     #-----------------------------------------------------------------------------
     # define all the get() method here:
@@ -88,6 +113,7 @@ class BACnetServer(object):
             return True
         else:
             print("setObjValue() Error: the object name %s is not exist" %str(objName))
+            return False
 
     #-----------------------------------------------------------------------------
     def runServer(self):
@@ -97,15 +123,27 @@ class BACnetServer(object):
 #-----------------------------------------------------------------------------
 #-----------------------------------------------------------------------------
 class BACnetClient(object):
-
-    def __init__(self, deviceID, deviceName, serverIP, serverPort, 
+    """ The BAC client module to simulate a console device connect to the BAC 
+        device to fetch the data. 
+    """
+    def __init__(self, deviceID, deviceName, serverIP, serverPort,
                  ipaddr=DEF_CLIENT_IP, port=DEF_CLIENT_PORT):
+        """ Init example: client = BACnetComm.BACnetClient(2, "TestClient", '127.0.0.1', 47808)
+
+        Args:
+            deviceID (init): unique console/HMI device ID.
+            deviceName (str): console/HMI device name.
+            serverIP (str): target server IP.
+            serverPort (int): target server Port.
+            ipaddr (str, optional): client own IP. Defaults to DEF_CLIENT_IP.
+            port (int, optional): client own port. Defaults to DEF_CLIENT_PORT.
+        """
         self.deviceID = int(deviceID)
         self.deviceName = str(deviceName)
         self.serverIP = "%s:%s" % (str(serverIP), str(serverPort))
         self.clientIP = "%s:%s" % (str(ipaddr), str(port))
         self.device = LocalDeviceObject(
-            objectName=self.deviceName ,
+            objectName=self.deviceName,
             objectIdentifier=self.deviceID,
             maxApduLengthAccepted=1024,
             segmentationSupported="segmentedBoth",
@@ -145,7 +183,16 @@ class BACnetClient(object):
     def readObjProperty(self, serverAddress, objectInstance, 
                         objectType=DEF_ANALOG_VALUE_TYPE, 
                         propertyIdentifier = DEF_PROPERTY_IDENTIFIER):
-        """Read a property from a BACnet object"""
+        """ Read a object's property from a target BACnet server. 
+            Args:
+                serverAddress (str): server IP + port string example: "127.0.0.1:47808" 
+                objectInstance (int): object instance ID.
+                objectType (str, optional): object type. Defaults to DEF_ANALOG_VALUE_TYPE.
+                propertyIdentifier (str, optional): property ID. Defaults to DEF_PROPERTY_IDENTIFIER.
+
+            Returns:
+                float: the analog value.
+        """
         try:
             # Reset response
             self.responseValue = None
@@ -172,46 +219,53 @@ class BACnetClient(object):
             return None
 
     #-----------------------------------------------------------------------------
-    def write_property(self, serverAddress, objectInstance, value, objectType=DEF_ANALOG_VALUE_TYPE, 
+    def writeObjProperty(self, serverAddress, objectInstance, value, objectType=DEF_ANALOG_VALUE_TYPE, 
                        propertyIdentifier = DEF_PROPERTY_IDENTIFIER):
-        #try:
+        """ Write a object's property from a target BACnet server. 
+            Args:
+                serverAddress (str): server IP + port string example: "127.0.0.1:47808" 
+                objectInstance (int): object instance ID.
+                value (float): value to write.
+                objectType (str, optional): object type. Defaults to DEF_ANALOG_VALUE_TYPE.
+                propertyIdentifier (str, optional): property ID. Defaults to DEF_PROPERTY_IDENTIFIER.
+
+            Returns:
+                float: the analog value.
+        """
+        try:
             # Reset response
-        self.responseValue = None
-        self.responseEvent.clear()
-        # Build the request
-        request = WritePropertyRequest(
-            objectIdentifier=(objectType, objectInstance),
-            propertyIdentifier=propertyIdentifier,
-            priority=16 
-        )
-        if serverAddress is not None: serverAddress = self.serverIP
-        request.pduDestination = Address(serverAddress)
-
-        # Convert the value to the appropriate type
-        request.propertyValue = Any()
-        request.propertyValue.cast_in(Real(value))
-
-        # Make an IOCB with callback
-        iocb = IOCB(request)
-        iocb.add_callback(self.responseCallback)
-        
-        # Submit the request
-        self.application.request_io(iocb)
-
-        # Wait for response (with timeout)
-        if self.responseEvent.wait(timeout=10.0):
-            if self.responseValue == "success":
-                print(f"   Successfully wrote {value} to {objectType}:{objectInstance}")
-                return True
+            self.responseValue = None
+            self.responseEvent.clear()
+            # Build the request
+            request = WritePropertyRequest(
+                objectIdentifier=(objectType, objectInstance),
+                propertyIdentifier=propertyIdentifier,
+                priority=16 
+            )
+            if serverAddress is not None: serverAddress = self.serverIP
+            request.pduDestination = Address(serverAddress)
+            # Convert the value to the appropriate type
+            request.propertyValue = Any()
+            request.propertyValue.cast_in(Real(value))
+            # Make an IOCB with callback
+            iocb = IOCB(request)
+            iocb.add_callback(self.responseCallback)
+            # Submit the request
+            self.application.request_io(iocb)
+            # Wait for response (with timeout)
+            if self.responseEvent.wait(timeout=10.0):
+                if self.responseValue == "success":
+                    print("writeObjProperty() Successfully wrote %s to obj %s" % (str(value), str(objectInstance)))
+                    return True
+                else:
+                    return False
             else:
+                print("writeObjProperty() Warning: Timeout waiting for write confirmation")
                 return False
-        else:
-            print("   Timeout waiting for write confirmation")
-            return False
 
-        #except Exception as e:
-        #    print("write_property() Write error: %s" %str(e))
-        #    return None
+        except Exception as e:
+            print("write_property() Write error: %s" %str(e))
+            return None
 
     #-----------------------------------------------------------------------------
     def cleanup(self):
@@ -266,8 +320,6 @@ def main():
     print("get the obj value: %s" % str(val))
     server.setObjValue('Temperature', 25.0)
     server.runServer()
-
-
 
 if __name__ == "__main__":
     main()
